c4ae68f669dbd695622fb611611919d41d402cad
gpsayers <garthsayers@yahoo.com>
2015-12-01 22:18:31 -0600

    Added game.js initial commit

--- js/game.js
+++ js/game.js
@@ -1,3 +1,29 @@
 /**
  * Created by Garth on 12/1/2015.
  */
+var gameProperties = {
+    screenWidth: 640,
+    screenHeight: 470
+};
+
+var mainState = function (game) {
+
+};
+
+mainState.prototype = {
+    preload: function() {
+
+    },
+    create: function() {
+
+    },
+    update: function () {
+
+    }
+};
+
+var game = new Phaser.Game(gameProperties.screenWidth, gameProperties.screenHeight,Phaser.AUTO,'gameDiv');
+
+game.state.add('main',mainState);
+
+game.state.start('main');
