d09c4aa8908a1a9bc32ceba9829a8b01c91d5d2c
gsayers <gsayers@discoverybenefits.com>
2015-12-04 17:01:59 -0600

    Battle update

--- js/battle.js
+++ js/battle.js
@@ -8,9 +8,11 @@ battleMain.prototype = {
     },
 
     create: function () {
-        battleEnemies.forEach(function(item){
-            var enemy = game.add.sprite(game.camera.width / 2, game.camera.height / 2, 'rpg' ,item);
-        })
+
+		
+		this.initBattleScene();
+		
+		this.initBattleMenu();
     },
 
     update: function () {
@@ -22,4 +24,24 @@ battleMain.prototype = {
             }
     },
 
+	
+	initBattleScene: function () {
+		battleEnemies.forEach(function(item){
+            var enemy = game.add.sprite(game.camera.width / 2, game.camera.height / 2, 'rpg' ,item);
+        })
+		
+		var wiz = game.add.sprite(100,100,'rpg', '');
+		
+		
+	},
+	
+	initBattleMenu: function () {
+		var txt = game.add.text(50,game.camera.height-20, 'Attack', {});
+		
+		txt.event.onInputUp.add(attack, this);
+	},
+	
+	attack: function() {
+		
+	},
 }
